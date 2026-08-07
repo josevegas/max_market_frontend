43f8d36dd64a77f01f8475e8133117f2ef68e918
unknown <josevegas.marquez@gmail.com>
2026-08-07 08:33:31 -0500

    corrigiendo cors

--- src/app/core/config/environment.prod.ts
+++ src/app/core/config/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://maxmarketapi-production.up.railway.app',
+  apiUrl: 'https://maxmarketapi-production.up.railway.app/api/v1',
 };
